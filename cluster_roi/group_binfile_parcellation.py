cf355fda773882a890b56aeed757b093874f421e
thisisamirv <thisisamirv@gmail.com>
2022-01-07 20:14:43 +0330

    one print function bug fixed

--- cluster_roi/group_binfile_parcellation.py
+++ cluster_roi/group_binfile_parcellation.py
@@ -132,7 +132,7 @@ def group_binfile_parcellate( infiles, outfile, K, n_voxels ):
     group_img=eigenvec_discrete[:,0]
 
     for i in range(1,K):
-    	if not i%10: print i
+    	if not i%10: print(i)
         group_img=group_img+(i+1)*eigenvec_discrete[:,i]
 
     save(outfile,group_img.todense())
